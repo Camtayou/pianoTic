148f34900e74ac634be74fe68b56300f09568bf5
KomilSam <165714259+KomilSam@users.noreply.github.com>
2024-04-03 09:57:20 +0400

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -7,32 +7,62 @@ import android.widget.ImageView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    // Créez une instance de la classe Piano
+    // Create an instance of the Piano class
     private val piano = Piano()
+
+    // Create a HashMap to associate each piano key with its corresponding ImageView
+    private val touchesMap = HashMap<ImageView, TouchePianoBasique>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Générer une touche lorsque l'activité est créée
+        // Set onClickListeners for each piano key
+        for (i in 1..4) {
+            val id = resources.getIdentifier("touche$i", "id", packageName)
+            val touche = findViewById<ImageView>(id)
+            touche.setOnClickListener {
+                println("OnClickListener triggered for key $i") // Debug message
+                if (piano != null) {
+                    val touchePiano = touchesMap[touche]
+                    if (touchePiano != null) {
+                        piano.presserTouche(i - 1, touche) // Call presserTouche on 'piano'
+                        touchePiano.disparaitre(touche) // Make the key disappear
+                    } else {
+                        println("touchePiano is null for key $i")
+                    }
+                } else {
+                    println("piano is null")
+                }
+            }
+        }
+
+        // Generate a key when the activity is created
         genererTouche()
     }
 
+
     fun genererTouche() {
-        // Choisissez une colonne aléatoire
+        // Choose a random column
         val colonne = Random.nextInt(4) + 1
 
-        // Trouvez l'ImageView correspondant à la colonne
+        // Find the ImageView corresponding to the column
         val id = resources.getIdentifier("touche$colonne", "id", packageName)
         val touche = findViewById<ImageView>(id)
 
-        // Créez une instance de la classe TouchePianoBasique
-        val touchePiano = TouchePianoBasique(colonne, piano)
+        // Create an instance of the TouchePianoBasique class
+        val touchePiano = TouchePianoBasique(colonne, this, piano)
 
-        // Ajoutez la touche au piano
+        // Add the key to the piano
         piano.ajouterTouche(touchePiano)
 
-        // Faites défiler la touche
+        // Add the key to the HashMap
+        touchesMap[touche] = touchePiano
+
+        // Scroll the key
         touchePiano.defiler(touche)
+    }
+    fun retirerToucheMap(touche: ImageView) {
+        touchesMap.remove(touche)
     }
 }
