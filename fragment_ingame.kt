330bd3faed45b1d1bbc11b9ad892221a77ede700
KomilSam <165714259+KomilSam@users.noreply.github.com>
2024-04-04 16:47:29 +0400

    Update fragment_ingame.kt

--- fragment_ingame.kt
+++ fragment_ingame.kt
@@ -8,7 +8,7 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import android.widget.LinearLayout
 
-class Fragmentingame : Fragment() {
+open class Fragmentingame : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,28 +17,35 @@ class Fragmentingame : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.ingame, container, false)
 
-        // Le code pour initier les blocs qui déscendent
-        // Get a reference to the content view (inflated layout)
-        val contentView = view.findViewById<View>(R.id.ingamepaccomplet) // Assuming your layout's ID is "ingamepaccomplet"
-
-        // Find the "Colonne4" view within the inflated content view
-        val colonne4 = contentView.findViewById<LinearLayout>(R.id.Colonne4)
-
-        // Create the BlackSquareScroller and pass the "Colonne4" view
-        blackSquareScroller = BlackSquareScroller(requireActivity(), colonne4) // Use requireActivity() for context
-        blackSquareScroller.start()
-
         view.findViewById<Button>(R.id.btn_arriere).setOnClickListener {
             fragmentManager?.beginTransaction()
                 ?.replace(R.id.fragment_container_view_tag, Fragment_homepage())
                 ?.commit()
         }
 
+        val pianoKeyContainer1 = view.findViewById<LinearLayout>(R.id.pianoKeyContainer1)
+        val pianoKeyContainer2 = view.findViewById<LinearLayout>(R.id.pianoKeyContainer2)
+        val pianoKeyContainer3 = view.findViewById<LinearLayout>(R.id.pianoKeyContainer3)
+        val pianoKeyContainer4 = view.findViewById<LinearLayout>(R.id.pianoKeyContainer4)
 
-        }
+        // Créez des instances de PianoKey avec les dimensions et les ressources d'image souhaitées
+        var pianoKey1 = PianoKey(requireContext(), null, 0, 100, 100, R.drawable.touchepiano1, 20f, 500f)
+        var pianoKey2 = PianoKey(requireContext(), null, 0, 100, 100, R.drawable.touchepiano1, 20f, 200f)
+        var pianoKey3 = PianoKey(requireContext(), null, 0, 100, 100, R.drawable.touchepiano1, 20f, 100f)
+        var pianoKey4 = PianoKey(requireContext(), null, 0, 100, 100, R.drawable.touchepiano1, 20f, 1000f)
+
+        // Ajoutez les objets PianoKey au LinearLayout
+        pianoKeyContainer1.addView(pianoKey1)
+        pianoKeyContainer2.addView(pianoKey2)
+        pianoKeyContainer3.addView(pianoKey3)
+        pianoKeyContainer4.addView(pianoKey4)
+
+        // Lancez l'animation pour chaque touche de piano
+        pianoKey1.startPianoKeyAnimation()
+        pianoKey2.startPianoKeyAnimation()
+        pianoKey3.startPianoKeyAnimation()
+        pianoKey4.startPianoKeyAnimation()
 
         return view
-
-
     }
 }
