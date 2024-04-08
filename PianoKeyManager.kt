905ad9828b2227e46354ee1f2be54f8488b54929
Camtayou <117365620+Camtayou@users.noreply.github.com>
2024-04-08 23:03:47 +0200

    Update PianoKeyManager.kt

--- PianoKeyManager.kt
+++ PianoKeyManager.kt
@@ -10,50 +10,92 @@ import android.widget.RelativeLayout
 
 class PianoKeyManager(private val context: Context,
                       private val pianoKeyContainers: List<RelativeLayout>,
-                      private val scoreViewModel: ScoreViewModel) {
+                      private val scoreViewModel: ScoreViewModel,
+                      private val pianoKeys: MutableList<PianoKey> = mutableListOf()) {
     private val handler = Handler(Looper.getMainLooper())
     private var lastColumnIndex = -1
+    var isGameRunning = true
+    private var isRunnablePosted = false
 
     private val runnable = object : Runnable {
         override fun run() {
             createPianoKeyInRandomColumn()
-            handler.postDelayed(this, 1000)
+            if (isGameRunning) {
+                handler.postDelayed(this, 1000)
+            } else {
+                isRunnablePosted = false
+            }
         }
     }
     fun startGeneratingPianoKeys() {
-        handler.post(runnable)
+        if (!isRunnablePosted) {
+            handler.post(runnable)
+            isRunnablePosted = true
+        }
     }
 
     fun stopGeneratingPianoKeys() {
         handler.removeCallbacks(runnable)
     }
+    fun pauseGame() {
+        isGameRunning = false
+        pianoKeys.forEach { pianoKey: PianoKey ->
+            pianoKey.pause()
+            pianoKey.disableClick()
+        }
+    }
+
+    fun resumeGame() {
+        isGameRunning = true
+        pianoKeys.forEach { pianoKey: PianoKey ->
+            pianoKey.resume()
+            pianoKey.enableClick()
+        }
+        startGeneratingPianoKeys()
+    }
+
 
     private fun createPianoKeyInRandomColumn() {
-        var randomColumnIndex: Int
-        do {
-            randomColumnIndex = Random.nextInt(pianoKeyContainers.size)
-        } while (randomColumnIndex == lastColumnIndex)
+        if (isGameRunning) {
+            var randomColumnIndex: Int
+            do {
+                randomColumnIndex = Random.nextInt(pianoKeyContainers.size)
+            } while (randomColumnIndex == lastColumnIndex)
 
-        lastColumnIndex = randomColumnIndex
+            lastColumnIndex = randomColumnIndex
 
-        val pianoKeyContainer = pianoKeyContainers[randomColumnIndex] as RelativeLayout
-        val pianoKey = PianoKey(context, null, 0, 100, 100, R.drawable.touchepiano1, -1000f, 3000f, scoreViewModel)
+            val pianoKeyContainer = pianoKeyContainers[randomColumnIndex] as RelativeLayout
+            val pianoKey = PianoKey(
+                context,
+                null,
+                0,
+                100,
+                100,
+                R.drawable.touchepiano1,
+                -1000f,
+                3000f,
+                scoreViewModel
+            )
+            // Pour lorsqu'on est en pause
+            pianoKeys.add(pianoKey)
 
-        // Get the height and width from the PianoKey instance
-        val keyHeight = pianoKey.keyHeight
-        val keyWidth = pianoKey.keyWidth
+            // Get the height and width from the PianoKey instance
+            val keyHeight = pianoKey.keyHeight
+            val keyWidth = pianoKey.keyWidth
 
-        // Convert the height and width to pixels
-        val scale = context.resources.displayMetrics.density
-        val heightInPx = (keyHeight * scale + 0.5f).toInt()
-        val widthInPx = (keyWidth * scale + 0.5f).toInt()
+            // Convert the height and width to pixels
+            val scale = context.resources.displayMetrics.density
+            val heightInPx = (keyHeight * scale + 0.5f).toInt()
+            val widthInPx = (keyWidth * scale + 0.5f).toInt()
 
-        // Use the height and width to set the RelativeLayout.LayoutParams
-        val params = RelativeLayout.LayoutParams(widthInPx, heightInPx)
-        params.addRule(RelativeLayout.ALIGN_PARENT_TOP)
-        pianoKey.layoutParams = params
+            // Use the height and width to set the RelativeLayout.LayoutParams
+            val params = RelativeLayout.LayoutParams(widthInPx, heightInPx)
+            params.addRule(RelativeLayout.ALIGN_PARENT_TOP)
+            pianoKey.layoutParams = params
 
-        pianoKeyContainer.addView(pianoKey)
-        pianoKey.startPianoKeyAnimation()
+            pianoKeyContainer.addView(pianoKey)
+            pianoKey.startPianoKeyAnimation()
+        }
     }
+
 }
