1bf3ee35f1c6fb22204e8d6cd18fe00a85392ace
Camtayou <117365620+Camtayou@users.noreply.github.com>
2024-04-17 15:45:34 +0200

    Update library.kt

--- library.kt
+++ library.kt
@@ -1,1 +1,19 @@
+package com.example.pianotiles
 
+import android.content.Context
+import android.widget.ImageView
+import androidx.fragment.app.FragmentActivity
+import com.example.pianotiles.PlayerStats.Companion
+
+class library(val context: Context, val prenom: String, val photo: Int) {
+    val imageView: ImageView = ImageView(context).apply {
+        setImageResource(photo)
+        setOnClickListener {
+            Companion.profileName = prenom
+            PlayerStats.profilePhoto = drawable
+            (context as FragmentActivity).supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view_tag, PlayerStatsFragment())
+                .commit()
+        }
+    }
+}
