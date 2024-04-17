516b2c85ca90233c5bf056cc5ae219d1e2a57390
Camtayou <117365620+Camtayou@users.noreply.github.com>
2024-04-17 15:50:14 +0200

    Update ScoreViewModel.kt

--- ScoreViewModel.kt
+++ ScoreViewModel.kt
@@ -1,25 +1,20 @@
 package com.example.pianotiles
 
-// ScoreViewModel.kt
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import java.util.*
 
-class ScoreViewModel : ViewModel() {
-    open var score = 0
+class ScoreViewModel private constructor() : ViewModel() {
+    var score: MutableLiveData<Int> = MutableLiveData(-1)
 
     private var timer: Timer? = null
-
-    fun incrementScore() {
-        score += 10
-    }
 
     fun startScoreTimer() {
         timer = Timer()
         timer?.scheduleAtFixedRate(object : TimerTask() {
             override fun run() {
-                score += 1
+                score.postValue((score.value ?: 0) + 1)
+                PlayerStats.Companion.totalScore += 1
             }
         }, 0, 1000)
     }
@@ -38,5 +33,21 @@ class ScoreViewModel : ViewModel() {
         startScoreTimer()
     }
 
+    companion object {
+        @Volatile
+        private var INSTANCE: ScoreViewModel? = null
 
+        fun getInstance(): ScoreViewModel {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = ScoreViewModel()
+                    INSTANCE = instance
+                }
+
+                return instance
+            }
+        }
+    }
 }
