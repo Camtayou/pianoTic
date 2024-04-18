3f0245c898eef065f54a92d0bb1192e0a58af159
Camtayou <117365620+Camtayou@users.noreply.github.com>
2024-04-18 13:52:37 +0200

    Update DarkModeHandler.kt

--- DarkModeHandler.kt
+++ DarkModeHandler.kt
@@ -6,44 +6,28 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import android.view.ViewGroup
+import android.content.Context
+import android.graphics.drawable.ColorDrawable
 
-/* class DarkModeHandler(private val fragment: Fragment, private val xmlpage : String) {
-
-
-
-    fun ViewGroup.updateTextColor(color: Int) {
-        for (i in 0 until childCount) {
-            val child = getChildAt(i)
-            if (child is TextView && child !is Button) {
-                child.setTextColor(color)
-            } else if (child is ViewGroup) {
-                child.updateTextColor(color)
+class DarkModeHandler {
+    fun updateAllViews(isDarkMode: Boolean, view: View, context: Context) {
+        if (view is ViewGroup && view !is Button ) {
+            for (i in 0 until view.childCount) {
+                val child = view.getChildAt(i)
+                updateAllViews(isDarkMode, child, context) // Recursive call for nested ViewGroup
             }
-        }
-    }
-
-    fun setupDarkMode(view: View) {
-        val btnDarkMode = view.findViewById<Button>(R.id.btn_darkmode)
-        btnDarkMode.setOnClickListener {
-            DarkMode.toggle()
-        }
-
-        DarkMode.addObserver(object : DarkModeObserver {
-            override fun onDarkModeChanged(isDarkMode: Boolean) {
-
-                val layoutId = fragment.resources.getIdentifier(xmlpage, "id", fragment.context?.packageName)
-                println(layoutId)
-                val layout = view.findViewById<LinearLayout>(layoutId)
-                println(layoutId)
-
-                val textColor = if (isDarkMode) ContextCompat.getColor(fragment.requireContext(), R.color.white) else ContextCompat.getColor(fragment.requireContext(), R.color.black)
-                val backgroundColor = if (isDarkMode) ContextCompat.getColor(fragment.requireContext(), R.color.black) else ContextCompat.getColor(fragment.requireContext(), R.color.white)
-
-                layout.setBackgroundColor(backgroundColor)
-                layout.updateTextColor(textColor)
+            val backgroundColor = if (isDarkMode) ContextCompat.getColor(context, R.color.black) else ContextCompat.getColor(context, R.color.white)
+            if (view.background is ColorDrawable) {
+                val currentBackgroundColor = (view.background as ColorDrawable).color
+                if (currentBackgroundColor == ContextCompat.getColor(context, R.color.white) || currentBackgroundColor == ContextCompat.getColor(context, R.color.black)) {
+                    view.setBackgroundColor(backgroundColor)
+                }
             }
-        })
+        } else if (view is TextView) {
+            val textColor = if (isDarkMode) ContextCompat.getColor(context, R.color.white) else ContextCompat.getColor(context, R.color.black)
+            view.setTextColor(textColor)
+        }
     }
 }
 
-*/
+
