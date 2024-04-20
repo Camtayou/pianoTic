8999c8cb3643351bab31b9ad7644a038fda67733
Camtayou <117365620+Camtayou@users.noreply.github.com>
2024-04-20 14:24:39 +0200

    Update fragment_homepage.kt

--- fragment_homepage.kt
+++ fragment_homepage.kt
@@ -16,52 +16,65 @@ import androidx.core.content.ContextCompat
 import android.service.autofill.Validators.and
 import android.content.Context
 
+// Définition de la classe Fragment_homepage qui hérite de Fragment
+class Fragment_homepage() : Fragment() {
 
-
-
-class Fragment_homepage : Fragment() {
-
-
+    // Méthode pour créer la vue du fragment
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate le layout pour ce fragment
         return inflater.inflate(R.layout.homepage, container, false)
     }
 
+    // Méthode appelée après que la vue du fragment a été créée
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Récupère le conteneur de la photo de profil
         val photoContainer1 = view.findViewById<LinearLayout>(R.id.container_profile1)
+        // Si une photo de profil a été définie,
         if (Companion.profilePhoto != null) {
+            // Crée une nouvelle ImageView et lui attribue la photo de profil
             val imageView = ImageView(context).apply {
                 setImageDrawable(Companion.profilePhoto)
             }
+            // Ajoute l'ImageView au conteneur de la photo de profil
             photoContainer1.addView(imageView)
         }
 
+        // Définit un OnClickListener pour le bouton de musique
         view.findViewById<Button>(R.id.musicClique1).setOnClickListener {
+            // Crée un nouveau SoundManager
+            val soundHandler = SoundManager(requireContext())
+            // Remplace le fragment actuel par un Fragmentingame
             fragmentManager?.beginTransaction()
                 ?.replace(R.id.fragment_container_view_tag, Fragmentingame())
                 ?.commit()
         }
 
+        // Définit un OnClickListener pour le bouton des statistiques
         view.findViewById<Button>(R.id.btn_stats).setOnClickListener {
+            // Remplace le fragment actuel par un PlayerStatsFragment
             fragmentManager?.beginTransaction()
                 ?.replace(R.id.fragment_container_view_tag, PlayerStatsFragment())
                 ?.commit()
         }
 
-
+        // Récupère le bouton du mode sombre
         val btn_darkmode = view.findViewById<Button>(R.id.btn_darkmode)
-        // Update the background color based on the current dark mode state
+        // Récupère le layout de la page d'accueil
         val layout1 = view.findViewById<ViewGroup>(R.id.homepage)
-
+        // Met à jour toutes les vues en fonction de l'état actuel du mode sombre
         DarkModeHandler().updateAllViews(DarkMode.isDarkMode, layout1, requireContext())
 
+        // Définit un OnClickListener pour le bouton du mode sombre
         btn_darkmode.setOnClickListener {
+            // Bascule l'état du mode sombre
             DarkMode.toggle()
+            // Met à jour toutes les vues en fonction du nouvel état du mode sombre
             DarkModeHandler().updateAllViews(DarkMode.isDarkMode, layout1, requireContext())
         }
     }
