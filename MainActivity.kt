92e507ff35ce305ad011062eac8df6edcac5915e
Camtayou <117365620+Camtayou@users.noreply.github.com>
2024-04-20 14:27:07 +0200

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -9,37 +9,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import androidx.fragment.app.FragmentManager
 
-class MainActivity : AppCompatActivity(), GameOverListener {
+// Définition de la classe MainActivity qui hérite de AppCompatActivity, implémente GameOverListener et SoundHandlerProvider
+class MainActivity : AppCompatActivity(), GameOverListener, SoundHandlerProvider {
+    // Déclaration des variables
     private lateinit var fragmentContainer: FragmentContainerView
     private lateinit var fragmentManager: FragmentManager
     private lateinit var gameOverFragment: Fragment
+    private val soundHandler = SoundManager(this)
 
+    // Méthode appelée lors de la création de l'activité
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Définit le layout pour cette activité
         setContentView(R.layout.activity_main)
 
+        // Initialise les variables
         fragmentContainer = findViewById(R.id.fragment_container_view_tag)
         fragmentManager = supportFragmentManager
-        gameOverFragment = gameover() as Fragment
 
-        // Add Fragment First to the container initially (if no savedInstanceState)
+        // Ajoute le Fragment_homepage au conteneur de fragments initialement (si aucun savedInstanceState)
         if (savedInstanceState == null) {
             val transaction = fragmentManager.beginTransaction()
             transaction.replace(R.id.fragment_container_view_tag, Fragment_homepage())
             transaction.commit()
         }
+
+        // Initialise le fragment de fin de partie
+        gameOverFragment = gameover()
     }
+
+    // Méthode appelée lorsque le jeu est terminé
     override fun onGameOver() {
         showGameOverScreen()
     }
+
+    // Méthode pour afficher l'écran de fin de partie
     fun showGameOverScreen() {
-        // Arrêtez le jeu
+        // Arrête le jeu
         // Remplacez par votre propre logique pour arrêter le jeu
 
-        // Affichez l'écran de fin de partie
+        // Affiche l'écran de fin de partie
         val transaction = fragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container_view_tag, gameover())
+        transaction.replace(R.id.fragment_container_view_tag, gameOverFragment)
         transaction.commit()
     }
 
+    // Méthode pour obtenir le gestionnaire de son
+    override fun getSoundHandler(): SoundHandler {
+        return soundHandler
+    }
 }
