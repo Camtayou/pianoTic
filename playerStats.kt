8ff71af6945ee40633121f4573ae8791eb2d9da9
Camtayou <117365620+Camtayou@users.noreply.github.com>
2024-04-20 14:29:57 +0200

    Update playerStats.kt

--- playerStats.kt
+++ playerStats.kt
@@ -2,20 +2,24 @@ package com.example.pianotiles
 
 import android.graphics.drawable.Drawable
 
+// Définition de la classe PlayerStats
 class PlayerStats {
     companion object {
+        // Déclaration des variables statiques
         var totalScore: Int = 0
         var totalPianoKeysPlayed: Int = 0
         var totalTimePlayed: Int = 0
         var profilePhoto: Drawable? = null
         var profileName: String? = null
 
+        // Méthode pour augmenter le nombre total de touches de piano jouées
         fun TotalKeyPressed (){
             totalPianoKeysPlayed++
         }
+
+        // Méthode pour augmenter le temps total joué
         fun TotalTimePlayed (){
             totalTimePlayed++
         }
     }
-
 }
