60410cb185f9c39d02bcda5d55733fd62df980d7
Camtayou <117365620+Camtayou@users.noreply.github.com>
2024-04-20 14:30:40 +0200

    Update ScoreViewModel.kt

--- ScoreViewModel.kt
+++ ScoreViewModel.kt
@@ -4,11 +4,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import java.util.*
 
+// Définition de la classe ScoreViewModel qui hérite de ViewModel
 class ScoreViewModel private constructor() : ViewModel() {
+    // Déclaration de la variable score en tant que MutableLiveData
     var score: MutableLiveData<Int> = MutableLiveData(-1)
 
+    // Déclaration de la variable timer
     private var timer: Timer? = null
 
+    // Méthode pour démarrer le timer de score
     fun startScoreTimer() {
         timer = Timer()
         timer?.scheduleAtFixedRate(object : TimerTask() {
@@ -18,30 +22,36 @@ class ScoreViewModel private constructor() : ViewModel() {
         }, 0, 1000)
     }
 
+    // Méthode pour ajouter des points au score
     fun addScore(points: Int) {
         val currentScore = score.value ?: 0
         score.postValue(currentScore + points)
         PlayerStats.Companion.totalScore += points
     }
 
+    // Méthode appelée lorsque le ViewModel est effacé
     override fun onCleared() {
         super.onCleared()
         timer?.cancel()
     }
 
+    // Méthode pour mettre en pause le timer de score
     fun pauseScoreTimer() {
         timer?.cancel()
         timer = null
     }
 
+    // Méthode pour reprendre le timer de score
     fun resumeScoreTimer() {
         startScoreTimer()
     }
 
+    // Objet compagnon pour le modèle singleton
     companion object {
         @Volatile
         private var INSTANCE: ScoreViewModel? = null
 
+        // Méthode pour obtenir l'instance de ScoreViewModel
         fun getInstance(): ScoreViewModel {
             synchronized(this) {
                 var instance = INSTANCE
